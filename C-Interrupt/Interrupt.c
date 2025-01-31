01601c736347263f7bfad0864b602f9f16b6af51
Tanishq Patil <trpatil@ucsc.edu>
2025-01-30 22:13:29 -0800

    adding files

--- C-Interrupt/Interrupt.c
+++ C-Interrupt/Interrupt.c
@@ -1,28 +1,51 @@
-#include "Interrupt.h"
-void gpio_interrupt() {
+#include "Interrupt.h"  
+
+void handle_gpio_interrupt() {
     int status = system("libcamera-still -o capture.jpg");
     if (status == -1) {
-        perror("system");
-        return 1;
+        perror("system err");
+        printf("Capture Failed\n");
     }
-    printf("capture occurred\n");
-    return 0;
-
-}
-
-void setup_gpio(){ // Setup gpio listener
-    wiringPiSetup();
-    wiringPiSetupGpio();
-    pinMode(GPIO_PIN, INPUT);
-    pullUpDnControl(GPIO_PIN, PUD_UP);
-
+    printf("Capture success!\n");
 }
 
 int main() {
-    setup_gpio();
-    writingPiISR(GPIO_PIN, INT_EDGE_FALLING, &gpio_interrupt);
-    while(1){
-        sleep(1);
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    struct gpiod_line_event event;
+    int ret;
+
+    chip = gpiod_chip_open(GPIO_CHIP);
+    if (!chip) {
+        perror("Failed to open GPIO chip");
+        return EXIT_FAILURE;
     }
-    return 0;
+
+    line = gpiod_chip_get_line(chip, GPIO_PIN);
+    if (!line) {
+        perror("Failed to get GPIO line");
+        gpiod_chip_close(chip);
+        return EXIT_FAILURE;
+    }
+    ret = gpiod_line_request_events(line, GPIOD_LINE_REQUEST_EVENT_FALLING);
+    if (ret < 0) {
+        perror("Failed to request events");
+        gpiod_chip_close(chip);
+        return EXIT_FAILURE;
+    }
+
+    printf("Waiting for GPIO interrupt on pin %d...\n", GPIO_PIN);
+    while (1) {
+        ret = gpiod_line_event_wait(line, NULL);
+        if (ret < 0) {
+            perror("Error waiting for event");
+            break;
+        } else if (ret > 0) {
+            gpiod_line_event_read(line, &event);
+            handle_gpio_interrupt();
+        }
+    }
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+    return EXIT_SUCCESS;
 }
