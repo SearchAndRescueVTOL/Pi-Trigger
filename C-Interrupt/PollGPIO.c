18924e766cfe3bcf1f570d7761012d00ba0481a7
trpatil007 <trpatil@ucsc.edu>
2025-02-27 00:06:18 -0800

    need to add UTC time calculation correctly

--- C-Interrupt/PollGPIO.c
+++ C-Interrupt/PollGPIO.c
@@ -10,20 +10,38 @@
 #include <stdatomic.h>
 #include <stdbool.h>
 #define DEFAULT_SAMPLE_RATE 1
+#define MAXVALTICK 4294967295
 pthread_barrier_t barrier;
 uint32_t tickGlobal;
 atomic_bool tickReady = ATOMIC_VAR_INIT(true);
 int trigger_counter = 0;
-uint32_t prev_tick = 0;
+long long prev_tick = 0;
+bool start = true;
 atomic_int recent_trigger_number = ATOMIC_VAR_INIT(0);
+struct timespec ts;
+FILE *fd;
+long long tickDiff(long long now, long long before){
+	if (now >= before){
+		return now - before;
+	}
+	else{
+		return now + (MAXVALTICK - before) + 1LL;
+	}
+}
 void aFunction(int gpio, int level, uint32_t tick) {
   /* only record low to high edges */
-  if (level == 1) {
+  
+if (level == 1) {
     trigger_counter += 1;
-    printf("Trigger Happened\n");
-	printf("Trigger %d\n", trigger_counter);
-	printf("Time: %d\n", tick);
-	// RGB time
+    timespec_get(&ts, TIME_UTC);
+    long long time = (long long) gpioTick();
+    long long utcTime = ((long long)ts.tv_sec * 1000LL + ts.tv_nsec / 1000000LL);
+    long long out = tickDiff(time, (long long) tick);
+    printf("%lld\n", utcTime);
+    
+    utcTime -= out;
+
+    fflush(fd);
     bool exp = true;
     bool desired = false;
     if (atomic_compare_exchange_strong(&tickReady, &exp, desired)){
@@ -44,6 +62,7 @@ void set_cpu_affinity(int core_id) {
   if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t),&cpuset) != 0) {
     perror("pthread_setaffinity_np");
   }
+  usleep(1000);
 }
 void *rgb_trigger(void *arg){
   int thread_id = *(int *)arg;
@@ -88,7 +107,7 @@ void *conductor(void *arg){
   pthread_t consume;
   pthread_attr_t attr;
   struct sched_param param;
-  set_cpu_affinity(2);
+  set_cpu_affinity(3);
   pthread_attr_init(&attr);
   pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
   param.sched_priority = sched_get_priority_max(SCHED_FIFO);
@@ -130,7 +149,7 @@ void *conductor(void *arg){
 }
 int main() {
 	// oracle code here
-   	
+   	fd = fopen("output.txt", "w");
 	pthread_t con;
 	if (pthread_create(&con, NULL, conductor, NULL) != 0){
 		printf("Error\n");
